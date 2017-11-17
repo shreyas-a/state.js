654635aa8e66b94e93506c70eb4dd8246919a1ff
shreyas-a <shreyas.a24@gmail.com>
2017-11-17 19:44:57 +0530

    basic reactivity

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-class State {
-  constructor() {
-    this.name = 'State JS';
-  }
+const addNotifier = (_object, key) => {
+  let val = _object[key];
 
-  getName() {
-    return this.name;
-  }
-}
+  Object.defineProperty(_object, key, {
+    get() {
+      return val;
+    },
+    set(newVal) {
+      val = newVal;
+      // notify(_key);
+    },
+  });
+};
+
+const State = {
+  create: _object => {
+    const clone = { ..._object };
+    Object.keys(clone).forEach(_key => {
+      addNotifier(clone, _key);
+    });
+    return clone;
+  },
+};
 
 module.exports = State;
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 // Plugins
-const Clean = require("clean-webpack-plugin");
+const Clean = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./index.js",
+  entry: './index.js',
   output: {
-    filename: "state.js",
-    path: path.resolve(__dirname, "./dist"),
+    filename: 'state.js',
+    path: path.resolve(__dirname, './dist'),
     library: 'State',
     libraryTarget: 'umd',
-    umdNamedDefine: true
+    umdNamedDefine: true,
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        use: "babel-loader"
-      }
-    ]
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ],
   },
   plugins: [
-    new Clean(["dist"], { root: process.cwd(), verbose: true }),
+    new Clean(['dist'], { root: process.cwd(), verbose: true }),
     new webpack.HashedModuleIdsPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
+      'process.env.NODE_ENV': '"production"',
     }),
     new webpack.optimize.UglifyJsPlugin({
       output: {
-        comments: false
+        comments: false,
       },
       compress: {
         warnings: false,
-        screw_ie8: true
+        screw_ie8: true,
       },
-      sourceMap: false
+      sourceMap: false,
     }),
     new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ]
+      minimize: true,
+    }),
+  ],
 };
